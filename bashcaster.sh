061a75188a15ad9f12bb8104523eabca42e54857
Noah Rosenzweig <noah@aware.vision>
2019-01-17 14:38:23 +0100

    Fix: set_with_slop function returns 0 when successful

--- bashcaster.sh
+++ bashcaster.sh
@@ -100,6 +100,7 @@ function set_to_screen_dimensions {
 function set_with_slop {
     check_program slop || return 1
     read left top width height < <(slop -f '%x %y %w %h')
+    return 0
 }
 
 function set_rectangle {
